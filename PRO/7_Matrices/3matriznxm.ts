d8a2210ade6de21b0928e47445b4a92b43ea2d7c
Nazav7 <nazarenavazquez07@gmail.com>
2024-06-15 21:01:24 -0300

    "funcion agregada en 3matriznxm"

--- PRO/7_Matrices/3matriznxm.ts
+++ PRO/7_Matrices/3matriznxm.ts
@@ -1,6 +1,9 @@
 /*• Crear una matriz de N filas y M columnas deseadas por el usuario
 • Completar la matriz con los números que el usuario ingrese
-• Mostrar el contenido de la matriz por pantalla*/
+• Mostrar el contenido de la matriz por pantalla:
+El valor de la celda 00 es 1
+...*/
+
 
 import * as rls from "readline-sync"
 
@@ -9,15 +12,22 @@ let columnsInput: number = rls.questionInt(`Ingrese cantidad de filas: `)
 let rowsInput: number = rls.questionInt(`Ingrese cantidad de columnas: `)
 
 // crear matriz con valores ingresados por el usuario
-let matrixNxM: number[][] = new Array(columnsInput)
-matrixNxM[0] = new Array(rowsInput)
-matrixNxM[1] = new Array(rowsInput)
+createMatrix(columnsInput, rowsInput)
 
-// llenar la matriz matrixNxM con valores ingresados por el usuario
+// llenar la matriz matrixNxM con dimensiones ingresadas por el usuario
 loadMatrixWithUserInput(matrixNxM)
 
 // mostrar la matriz por pantalla
 printMatrix(matrixNxM)
+
+//funcion para crear matriz con dimensiones ingresadas por el usuario
+function createMatrix(columns: number, rows: number): number[][] {
+    let matrix: number[][] = new Array(columns);
+    for (let i = 0; i < columns; i++) {
+        matrix[i] = new Array(rows);
+    }
+    return matrix;
+}
 
 //funcion para cargar matriz con valores ingresados por el usuario
 function loadMatrixWithUserInput(matrix: number[][]): number[][] {
